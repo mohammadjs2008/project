56057590f1f2c0ef721b06a681129b0f09349d9b
mohammad <mohammadjs2008@gmail.com>
2024-01-08 23:01:30 +0330

    add pr

--- js/script.js
+++ js/script.js
@@ -1,21 +1,6 @@
-var nam1 = prompt("معدلت چنده",0)
-
-switch (nam1){
-    case '18' :
-    case '19' :
-    case '20' :
-     alert("a") 
-     break
-    case '15' :
-    case '16' : 
-    case '17' :
-     alert("b")
-     break 
-    case '12' :
-    case '13' :
-    case '14' :
-     alert("c")
-     break
-     default:
-        alert("شما مشروطی برادر")
+function sumnumbers(nam1,nam2){
+    var resalt = nam1+ nam2
+    alert("resalt=" + resalt)
 }
+
+sumnumbers(5,9)
